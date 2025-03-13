607267c09d1fce485a3b66f4f98d310c208cf2a6
Valdez Gutierrez Aldo Eduardo <avaldez0@ucol.mx>
2025-03-13 13:42:16 -0600

    Set base path for relative routing in production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  base: './',  // 👈 Asegura rutas relativas para producción
   plugins: [
     vue(),
     vuetify({ autoImport: true }), 
